50ec25cc26e167deb2c87b7baa94928c2047e6a4
Patrick Mariano <99706405+patrickmariano04@users.noreply.github.com>
2023-02-09 11:55:50 +0800

    Update and rename home.php to index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
+<!--         <link href="css/styles.css" rel="stylesheet" /> -->
     </head>
     <body class="d-flex flex-column h-100">
         <main class="flex-shrink-0">
@@ -25,7 +25,7 @@
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                             <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
                             <li class="nav-item"><a class="nav-link" href="list.php">List of Restaurants</a></li>
                         </ul>
